9aaf75e58f44706cfecc0750a8aeb2caf3c9324f
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-03-21 03:14:26 -0600

    chore: Add Checkbox component to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
 // Supports weights 100-900
 import '@fontsource-variable/jost';
-export { Button } from "./Button/Button" 
+export { Button } from "./Button/Button"
+export { Checkbox } from "./Checkbox/Checkbox"
