26f83ead18b323730ddb74d1005cefb5e063be5c
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-03-21 18:01:28 -0600

    chore: Refactor Input component styles and structure

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,76 +1,76 @@
 import { ButtonHTMLAttributes, ReactNode, forwardRef, isValidElement } from "react";
-import styles from "./button.module.css"
+import styles from "./button.module.css";
 import { cva, VariantProps } from "class-variance-authority";
 import { Loader } from "lucide-react";
+import clsx from 'clsx';
 
-const button = cva(styles[`l-btn`], {
+const button = cva(styles[`lambda-btn`], {
     variants: {
         color: {
-            primary: styles.color_primary,
-            secondary: styles.color_secondary,
-            danger: styles.color_danger,
-            success: styles.color_success,
-            warning: styles.color_warning,
-            info: styles.color_info,
+            primary: styles[`lambda-btn--color-primary`],
+            secondary: styles[`lambda-btn--color-secondary`],
+            danger: styles[`lambda-btn--color-danger`],
+            success: styles[`lambda-btn--color-success`],
+            warning: styles[`lambda-btn--color-warning`],
+            info: styles[`lambda-btn--color-info`],
         },
         variant: {
-            solid: styles.variant_solid,
-            outline: styles.variant_outline,
-            dashed: styles.variant_dashed,
-            ghost: styles.variant_ghost,
+            solid: styles[`lambda-btn--variant-solid`],
+            outline: styles[`lambda-btn--variant-outline`],
+            dashed: styles[`lambda-btn--variant-dashed`],
+            ghost: styles[`lambda-btn--variant-ghost`],
         },
         size: {
-            small: styles.size_sm,
-            medium: styles.size_md,
-            large: styles.size_lg
-
+            small: styles[`lambda-btn--size-small`],
+            medium: styles[`lambda-btn--size-medium`],
+            large: styles[`lambda-btn--size-large`],
         },
         radius: {
-            none: styles.radius_none,
-            small: styles.radius_small,
-            medium: styles.radius_medium,
-            large: styles.radius_large,
-            pill: styles.radius_pill,
-            circle: styles.radius_circle
+            none: styles[`lambda-btn--radius-none`],
+            small: styles[`lambda-btn--radius-small`],
+            medium: styles[`lambda-btn--radius-medium`],
+            large: styles[`lambda-btn--radius-large`],
+            pill: styles[`lambda-btn--radius-pill`],
+            circle: styles[`lambda-btn--radius-circle`],
         },
-        icon_position: {
-            left: styles.icon_left,
-            right: styles.icon_right,
+        iconPosition: {
+            left: styles[`lambda-btn--icon-left`],
+            right: styles[`lambda-btn--icon-right`],
         },
         loading: {
-            true: styles[`l-btn-loading`],
-            false: styles.not_loading
+            true: styles[`lambda-btn--loading-true`],
+            false: styles[`lambda-btn--loading-false`],
         },
         disabled: {
-            false: styles.enabled,
-            true: styles.disabled,
+            false: styles[`lambda-btn--disabled-false`],
+            true: styles[`lambda-btn--disabled-true`],
         },
     },
     compoundVariants: [
-        { color: "primary", variant: 'solid', className: styles.primary_solid },
-        { color: "primary", variant: 'outline', className: styles.primary_outline },
-        { color: "primary", variant: 'dashed', className: styles.primary_dashed },
-        { color: "primary", variant: 'ghost', className: styles.primary_ghost },
-        { color: "secondary", variant: 'solid', className: styles.secondary_solid },
-        { color: "secondary", variant: 'outline', className: styles.secondary_outline },
-        { color: "secondary", variant: 'dashed', className: styles.secondary_dashed },
-        { color: "secondary", variant: 'ghost', className: styles.secondary_ghost },
-        { color: "danger", variant: 'solid', className: styles.danger_solid },
-        { color: "danger", variant: 'outline', className: styles.danger_outline },
-        { color: "danger", variant: 'dashed', className: styles.danger_dashed },
-        { color: "danger", variant: 'ghost', className: styles.danger_ghost },
-        { color: "success", variant: 'solid', className: styles.success_solid },
-        { color: "success", variant: 'outline', className: styles.success_outline },
-        { color: "success", variant: 'dashed', className: styles.success_dashed },
-        { color: "success", variant: 'ghost', className: styles.success_ghost },
-        { color: "warning", variant: 'solid', className: styles.warning_solid },
-        { color: "warning", variant: 'outline', className: styles.warning_outline },
-        { color: "warning", variant: 'dashed', className: styles.warning_dashed },
-        { color: "warning", variant: 'ghost', className: styles.warning_ghost },
-        { color: "info", variant: 'solid', className: styles.info_solid },
-        { color: "info", variant: 'outline', className: styles.info_outline },
-        { color: "info", variant: 'dashed', className: styles.info_dashed },
-        { color: "info", variant: 'ghost', className: styles.info_ghost },
+        { color: "primary", variant: 'solid', className: styles[`lambda-btn--primary-solid`] },
+        { color: "primary", variant: 'outline', className: styles[`lambda-btn--primary-outline`] },
+        { color: "primary", variant: 'dashed', className: styles[`lambda-btn--primary-dashed`] },
+        { color: "primary", variant: 'ghost', className: styles[`lambda-btn--primary-ghost`] },
+        { color: "secondary", variant: 'solid', className: styles[`lambda-btn--secondary-solid`] },
+        { color: "secondary", variant: 'outline', className: styles[`lambda-btn--secondary-outline`] },
+        { color: "secondary", variant: 'dashed', className: styles[`lambda-btn--secondary-dashed`] },
+        { color: "secondary", variant: 'ghost', className: styles[`lambda-btn--secondary-ghost`] },
+        { color: "danger", variant: 'solid', className: styles[`lambda-btn--danger-solid`] },
+        { color: "danger", variant: 'outline', className: styles[`lambda-btn--danger-outline`] },
+        { color: "danger", variant: 'dashed', className: styles[`lambda-btn--danger-dashed`] },
+        { color: "danger", variant: 'ghost', className: styles[`lambda-btn--danger-ghost`] },
+        { color: "success", variant: 'solid', className: styles[`lambda-btn--success-solid`] },
+        { color: "success", variant: 'outline', className: styles[`lambda-btn--success-outline`] },
+        { color: "success", variant: 'dashed', className: styles[`lambda-btn--success-dashed`] },
+        { color: "success", variant: 'ghost', className: styles[`lambda-btn--success-ghost`] },
+        { color: "warning", variant: 'solid', className: styles[`lambda-btn--warning-solid`] },
+        { color: "warning", variant: 'outline', className: styles[`lambda-btn--warning-outline`] },
+        { color: "warning", variant: 'dashed', className: styles[`lambda-btn--warning-dashed`] },
+        { color: "warning", variant: 'ghost', className: styles[`lambda-btn--warning-ghost`] },
+        { color: "info", variant: 'solid', className: styles[`lambda-btn--info-solid`] },
+        { color: "info", variant: 'outline', className: styles[`lambda-btn--info-outline`] },
+        { color: "info", variant: 'dashed', className: styles[`lambda-btn--info-dashed`] },
+        { color: "info", variant: 'ghost', className: styles[`lambda-btn--info-ghost`] },
     ],
     defaultVariants: {
         color: "primary",
@@ -78,20 +78,15 @@ const button = cva(styles[`l-btn`], {
         size: "medium",
         radius: 'medium',
         disabled: false,
-        icon_position: "left",
-        loading: false
+        iconPosition: "left",
+        loading: false,
     },
-})
+});
 
-
-
-export interface ButtonProps
-    extends Omit<ButtonHTMLAttributes<HTMLButtonElement>, "disabled" | "color">,
-    VariantProps<typeof button> {
+export interface ButtonProps extends Omit<ButtonHTMLAttributes<HTMLButtonElement>, "disabled" | "color">, VariantProps<typeof button> {
     icon?: ReactNode | undefined | null;
     label?: string;
 }
-
 
 export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
     (
@@ -103,34 +98,40 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
             radius = "small",
             icon,
             label,
-            icon_position = "left",
+            iconPosition = "left",
             loading,
             disabled,
             ...props
         },
         ref
     ) => {
-
         return (
             <button
                 ref={ref}
-                className={`${button({
-                    variant,
-                    size,
-                    color,
-                    disabled,
-                    radius,
-                    loading,
-                    icon_position,
-                    className,
-                })} ${(isValidElement<ReactNode>(props.children) || label) ? "" : styles["l-btn-only-icon"]}`}
+                className={clsx(
+                    button({
+                        variant,
+                        size,
+                        color,
+                        disabled,
+                        radius,
+                        loading,
+                        iconPosition,
+                        className,
+                    }),
+                    { [styles["lambda-btn--only-icon"]]: !(isValidElement<ReactNode>(props.children) || label) }
+                )}
                 disabled={disabled || undefined}
                 {...props}
             >
-                {(icon || loading) && <span className={styles["l-btn-icon"]}>{loading ? <Loader className={styles["l-btn-icon-loading"]} /> : icon}</span>}
-                {label && <span className={styles["l-btn-children"]}>{label}</span>}
+                {(icon || loading) && (
+                    <span className={styles["lambda-btn-icon"]}>
+                        {loading ? <Loader className={styles["lambda-btn-icon--loading"]} /> : icon}
+                    </span>
+                )}
+                {label && <span className={styles["lambda-btn-children"]}>{label}</span>}
                 {props.children}
             </button>
-        )
+        );
     }
-)
+);
