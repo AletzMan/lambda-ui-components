78f58bbdc6cac752cc528c1970d56dc40b3690ec
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-03-26 20:28:05 -0600

    refactor: Update landing page styles to adjust gap and padding

--- src/InputNumber/InputNumber.tsx
+++ src/InputNumber/InputNumber.tsx
@@ -4,40 +4,40 @@ import { cva, VariantProps } from "class-variance-authority"
 import { ChevronDown, ChevronUp, EuroIcon, DollarSignIcon, PercentIcon, PoundSterlingIcon } from "lucide-react"
 import clsx from "clsx"
 
-const inputNumber = cva(styles.lambda_number, {
+const inputNumber = cva(styles["lambda-number"], {
     variants: {
         size: {
-            tiny: styles['lambda_number--size-tiny'],
-            small: styles['lambda_number--size-small'],
-            medium: styles['lambda_number--size-medium'],
-            large: styles['lambda_number--size-large'],
+            tiny: styles['lambda-number-tiny'],
+            small: styles['lambda-number-small'],
+            medium: styles['lambda-number-medium'],
+            large: styles['lambda-number-large'],
         },
         variant: {
-            outline: styles['lambda_number--variant-outline'],
-            flat: styles['lambda_number--variant-flat'],
+            outline: styles['lambda-number-outline'],
+            flat: styles['lambda-number-flat'],
         },
         typeNumber: {
-            default: styles['lambda_number--type-default'],
-            'currency-USD': styles['lambda_number--type-usd'],
-            'currency-EUR': styles['lambda_number--type-eur'],
-            'currency-GBP': styles['lambda_number--type-gbp'],
-            percentage: styles['lambda_number--type-porcentage'],
-            decimal: styles['lambda_number--type-decimal'],
+            default: styles['lambda-number-default'],
+            'currency-USD': styles['lambda-number-usd'],
+            'currency-EUR': styles['lambda-number-eur'],
+            'currency-GBP': styles['lambda-number-gbp'],
+            percentage: styles['lambda-number-porcentage'],
+            decimal: styles['lambda-number-decimal'],
         },
         radius: {
-            none: styles['lambda_number--radius-none'],
-            small: styles['lambda_number--radius-small'],
-            medium: styles['lambda_number--radius-medium'],
-            large: styles['lambda_number--radius-large'],
-            pill: styles['lambda_number--radius-pill'],
+            none: styles['lambda-number-radius-none'],
+            small: styles['lambda-number-radius-small'],
+            medium: styles['lambda-number-radius-medium'],
+            large: styles['lambda-number-radius-large'],
+            pill: styles['lambda-number-radius-pill'],
         },
         invalid: {
-            true: styles['lambda_number--invalid'],
+            true: styles['lambda-number-invalid'],
             false: "",
         },
         disabled: {
-            false: styles['lambda_number--enabled'],
-            true: styles['lambda_number--disabled'],
+            false: styles['lambda-number-enabled'],
+            true: styles['lambda-number-disabled'],
         },
     },
     defaultVariants: {
@@ -50,20 +50,20 @@ const inputNumber = cva(styles.lambda_number, {
     },
 })
 
-const labels = cva(styles['lambda_number--label'], {
+const labels = cva(styles['lambda-number-label'], {
     variants: {
         size: {
-            tiny: styles['lambda_number--label-tiny'],
-            small: styles['lambda_number--label-small'],
-            medium: styles['lambda_number--label-medium'],
-            large: styles['lambda_number--label-large'],
+            tiny: styles['lambda-number-label-tiny'],
+            small: styles['lambda-number-label-small'],
+            medium: styles['lambda-number-label-medium'],
+            large: styles['lambda-number-label-large'],
         },
         radius: {
-            none: styles['lambda_number--label-radius-none'],
-            small: styles['lambda_number--label-radius-small'],
-            medium: styles['lambda_number--label-radius-medium'],
-            large: styles['lambda_number--label-radius-large'],
-            pill: styles['lambda_number--label-radius-pill'],
+            none: styles['lambda-number-label-radius-none'],
+            small: styles['lambda-number-label-radius-small'],
+            medium: styles['lambda-number-label-radius-medium'],
+            large: styles['lambda-number-label-radius-large'],
+            pill: styles['lambda-number-label-radius-pill'],
         },
     },
     defaultVariants: {
@@ -72,21 +72,21 @@ const labels = cva(styles['lambda_number--label'], {
     },
 })
 
-const number = cva(styles['lambda_number--field'], {
+const number = cva(styles['lambda-number-field'], {
     variants: {
         size: {
-            tiny: styles['lambda_number--field-tiny'],
-            small: styles['lambda_number--field-small'],
-            medium: styles['lambda_number--field-medium'],
-            large: styles['lambda_number--field-large'],
+            tiny: styles['lambda-number-field-tiny'],
+            small: styles['lambda-number-field-small'],
+            medium: styles['lambda-number-field-medium'],
+            large: styles['lambda-number-field-large'],
         },
         typeNumber: {
-            default: styles['lambda_number--field-default'],
-            'currency-USD': styles['lambda_number--field-usd'],
-            'currency-EUR': styles['lambda_number--field-eur'],
-            'currency-GBP': styles['lambda_number--field-gbp'],
-            percentage: styles['lambda_number--field-porc'],
-            decimal: styles['lambda_number--field-dec'],
+            default: styles['lambda-number-field-default'],
+            'currency-USD': styles['lambda-number-field-usd'],
+            'currency-EUR': styles['lambda-number-field-eur'],
+            'currency-GBP': styles['lambda-number-field-gbp'],
+            percentage: styles['lambda-number-field-porc'],
+            decimal: styles['lambda-number-field-dec'],
         },
     },
     defaultVariants: {
@@ -95,17 +95,17 @@ const number = cva(styles['lambda_number--field'], {
     },
 })
 
-const handler = cva(styles['lambda_number--handler'], {
+const handler = cva(styles['lambda-number-handler'], {
     variants: {
         size: {
-            tiny: styles['lambda_number--handler-tiny'],
-            small: styles['lambda_number--handler-small'],
-            medium: styles['lambda_number--handler-medium'],
-            large: styles['lambda_number--handler-large'],
+            tiny: styles['lambda-number-handler-tiny'],
+            small: styles['lambda-number-handler-small'],
+            medium: styles['lambda-number-handler-medium'],
+            large: styles['lambda-number-handler-large'],
         },
         variant: {
-            outline: styles['lambda_number--handler-outline'],
-            flat: styles['lambda_number--handler-flat'],
+            outline: styles['lambda-number-handler-outline'],
+            flat: styles['lambda-number-handler-flat'],
         }
     },
     defaultVariants: {
@@ -114,13 +114,13 @@ const handler = cva(styles['lambda_number--handler'], {
     },
 })
 
-const errorlabel = cva(styles['lambda_number--error'], {
+const errorlabel = cva(styles['lambda-number-error'], {
     variants: {
         size: {
-            tiny: styles['lambda_number--error-tiny'],
-            small: styles['lambda_number--error-small'],
-            medium: styles['lambda_number--error-medium'],
-            large: styles['lambda_number--error-large'],
+            tiny: styles['lambda-number-error-tiny'],
+            small: styles['lambda-number-error-small'],
+            medium: styles['lambda-number-error-medium'],
+            large: styles['lambda-number-error-large'],
         },
     },
     defaultVariants: {
@@ -128,25 +128,25 @@ const errorlabel = cva(styles['lambda_number--error'], {
     },
 })
 
-const typeCurrency = cva(styles['lambda_number--currency'], {
+const typeCurrency = cva(styles['lambda-number-currency'], {
     variants: {
         size: {
-            tiny: styles['lambda_number--currency-tiny'],
-            small: styles['lambda_number--currency-small'],
-            medium: styles['lambda_number--currency-medium'],
-            large: styles['lambda_number--currency-large'],
+            tiny: styles['lambda-number-currency-tiny'],
+            small: styles['lambda-number-currency-small'],
+            medium: styles['lambda-number-currency-medium'],
+            large: styles['lambda-number-currency-large'],
         },
         variant: {
-            outline: styles['lambda_number--currency-outline'],
-            flat: styles['lambda_number--currency-flat'],
+            outline: styles['lambda-number-currency-outline'],
+            flat: styles['lambda-number-currency-flat'],
         },
         typeNumber: {
-            default: styles['lambda_number--currency-dafault'],
-            'currency-USD': styles['lambda_number--currency-usd'],
-            'currency-EUR': styles['lambda_number--currency-eur'],
-            'currency-GBP': styles['lambda_number--currency-gbp'],
-            percentage: styles['lambda_number--currency-porc'],
-            decimal: styles['lambda_number--currency-dec'],
+            default: styles['lambda-number-currency-dafault'],
+            'currency-USD': styles['lambda-number-currency-usd'],
+            'currency-EUR': styles['lambda-number-currency-eur'],
+            'currency-GBP': styles['lambda-number-currency-gbp'],
+            percentage: styles['lambda-number-currency-porc'],
+            decimal: styles['lambda-number-currency-dec'],
         },
     },
     defaultVariants: {
@@ -156,13 +156,13 @@ const typeCurrency = cva(styles['lambda_number--currency'], {
     },
 })
 
-const button = cva(styles['lambda_number--btn'], {
+const button = cva(styles['lambda-number-btn'], {
     variants: {
         size: {
-            tiny: styles['lambda_number--btn-tiny'],
-            small: styles['lambda_number--btn-small'],
-            medium: styles['lambda_number--btn-medium'],
-            large: styles['lambda_number--btn-large'],
+            tiny: styles['lambda-number-btn-tiny'],
+            small: styles['lambda-number-btn-small'],
+            medium: styles['lambda-number-btn-medium'],
+            large: styles['lambda-number-btn-large'],
         },
     },
     defaultVariants: {
@@ -278,13 +278,13 @@ export const InputNumber = forwardRef<HTMLInputElement, InputProps>(
         const getIcon = () => {
             switch (typeNumber) {
                 case "currency-USD":
-                    return <DollarSignIcon className={styles['lambda_number--currency-icon']} />
+                    return <DollarSignIcon className={styles['lambda-number-currency-icon']} />
                 case "currency-EUR":
-                    return <EuroIcon className={styles['lambda_number--currency-icon']} />
+                    return <EuroIcon className={styles['lambda-number-currency-icon']} />
                 case "currency-GBP":
-                    return <PoundSterlingIcon className={styles['lambda_number--currency-icon']} />
+                    return <PoundSterlingIcon className={styles['lambda-number-currency-icon']} />
                 case "percentage":
-                    return <PercentIcon className={styles['lambda_number--currency-icon']} />
+                    return <PercentIcon className={styles['lambda-number-currency-icon']} />
                 default:
                     return null
             }
@@ -293,11 +293,11 @@ export const InputNumber = forwardRef<HTMLInputElement, InputProps>(
         const displayedValue = isEditing ? internalValue : formatValue(value)
 
         return (
-            <div className={clsx(styles['lambda_number--wrapper'], { [styles['lambda_number--wrapper-disabled']]: disabled })}>
+            <div className={clsx(styles['lambda-number-wrapper'], { [styles['lambda-number-wrapper-disabled']]: disabled })}>
                 {label && <label className={labels({ radius, size })}>{label}</label>}
                 <div className={inputNumber({ variant, disabled, radius, typeNumber, size, invalid, className })}>
-                    {helperText && <label className={clsx(styles['lambda_number--helper'], { [styles["lambda_number--helper-disabled"]]: disabled })}>{helperText}</label>}
-                    <div className={styles['lambda_number--container']}>
+                    {helperText && <label className={clsx(styles['lambda-number-helper'], { [styles["lambda-number-helper-disabled"]]: disabled })}>{helperText}</label>}
+                    <div className={styles['lambda-number-container']}>
                         <div className={typeCurrency({ typeNumber, size, variant })}>{getIcon()}</div>
                         <input
                             ref={ref}
@@ -318,21 +318,21 @@ export const InputNumber = forwardRef<HTMLInputElement, InputProps>(
                         <div className={handler({ size, variant })}>
                             <button
                                 type="button"
-                                className={clsx(button({ size }), styles['lambda_number--btn-increment'])}
+                                className={clsx(button({ size }), styles['lambda-number-btn-increment'])}
                                 onClick={increment}
                                 disabled={disabled || (max !== undefined && Number(value) >= Number(max))}
                             >
-                                <ChevronUp className={styles['lambda_number--icon']} />
+                                <ChevronUp className={styles['lambda-number-icon']} />
                             </button>
-                            <hr className={styles['lambda_number--separator']} />
+                            <hr className={styles['lambda-number-separator']} />
                             <button
                                 type="button"
-                                className={clsx(button({ size }), styles['lambda_number--btn-decrement'])}
+                                className={clsx(button({ size }), styles['lambda-number-btn-decrement'])}
 
                                 onClick={decrement}
                                 disabled={disabled || (min !== undefined && Number(value) <= Number(min))}
                             >
-                                <ChevronDown className={styles['lambda_number--icon']} />
+                                <ChevronDown className={styles['lambda-number-icon']} />
                             </button>
                         </div>
                     </div>
