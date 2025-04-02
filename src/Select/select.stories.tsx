27aa3479d323026fb4c67119d118d4743bb3f65e
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-04-02 00:20:29 -0600

    feat: Add Normal, Description, and Icon variations to Select component

--- src/Select/select.stories.tsx
+++ src/Select/select.stories.tsx
@@ -47,7 +47,7 @@ export default meta;
 
 type Story = StoryObj<typeof Select>
 
-export const Primary: Story = {
+export const Normal: Story = {
     args: {
         variant: "outline",
         radius: "small",
@@ -56,36 +56,103 @@ export const Primary: Story = {
         options: [
             {
                 label: "JavaScript",
-                value: "javascript"
+                value: "javascript",
             },
             {
                 label: "TypeScript",
-                value: "typescript"
+                value: "typescript",
             },
             {
                 label: "React",
-                value: "react"
+                value: "react",
             },
             {
                 label: "Node.js",
-                value: "nodejs"
+                value: "nodejs",
             },
             {
                 label: "C#",
-                value: "csharp"
+                value: "csharp",
+            }
+        ],
+        error: false,
+        errorMessage: "This is a sample error message for demonstration",
+        disabled: false,
+    },
+}
+
+export const Description: Story = {
+    args: {
+        variant: "outline",
+        radius: "small",
+        size: 'medium',
+        label: 'Label',
+        options: [
+            {
+                label: "JavaScript",
+                value: "javascript",
+                description: "A versatile programming language commonly used for web development."
+            },
+            {
+                label: "TypeScript",
+                value: "typescript",
+                description: "A superset of JavaScript that adds static typing for better security and scalability."
             },
             {
                 label: "React",
-                value: "react"
+                value: "react",
+                description: "A JavaScript library for building interactive and dynamic user interfaces."
             },
             {
                 label: "Node.js",
-                value: "nodejs"
+                value: "nodejs",
+                description: "A JavaScript runtime environment for building server-side applications."
             },
             {
                 label: "C#",
-                value: "csharp"
+                value: "csharp",
+                description: "A modern, object-oriented programming language developed by Microsoft."
+            }
+        ],
+        error: false,
+        errorMessage: "This is a sample error message for demonstration",
+        disabled: false,
+    },
+}
+
+
+export const Icon: Story = {
+    args: {
+        variant: "outline",
+        radius: "small",
+        size: 'medium',
+        label: 'Label',
+        options: [
+            {
+                label: "JavaScript",
+                value: "javascript",
+                avatar: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/240px-JavaScript-logo.png",
             },
+            {
+                label: "TypeScript",
+                value: "typescript",
+                avatar: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/250px-Typescript_logo_2020.svg.png",
+            },
+            {
+                label: "React",
+                value: "react",
+                avatar: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/330px-React-icon.svg.png",
+            },
+            {
+                label: "Node.js",
+                value: "nodejs",
+                avatar: "https://nodejs.org/static/logos/jsIconGreen.svg",
+            },
+            {
+                label: "C#",
+                value: "csharp",
+                avatar: "https://cdn.worldvectorlogo.com/logos/c--4.svg",
+            }
         ],
         error: false,
         errorMessage: "This is a sample error message for demonstration",
