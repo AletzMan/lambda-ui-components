bc28b37a3767e319e40b3c70137e65244a4acddd
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-04-10 23:24:22 -0600

    feat: Add HelperText component with size, disabled, and focused support

---
+++ src/_util/HelperText/HelperText.tsx
@@ -0,0 +1,38 @@
+import { cva } from "class-variance-authority"
+import styles from "./styles.module.css"
+import clsx from "clsx"
+
+const helper = cva(styles["lambda-helper"], {
+    variants: {
+        size: {
+            tiny: styles["lambda-helper-tiny"],
+            small: styles["lambda-helper-small"],
+            medium: styles["lambda-helper-medium"],
+            large: styles["lambda-helper-large"],
+        },
+        disabled: {
+            false: styles["lambda-helper-enabled"],
+            true: styles["lambda-helper-disabled"],
+        },
+    },
+    defaultVariants: {
+        size: "medium",
+        disabled: false
+    }
+})
+
+
+interface HelperTextProps {
+    text: string
+    size?: "tiny" | "small" | "medium" | "large" | null
+    disabled?: boolean | null
+    focused?: boolean | null
+}
+
+export function HelperText({ text, size = "medium", disabled = false, focused }: HelperTextProps) {
+    return (
+        <label className={clsx(helper({ disabled, size }), { [styles["lambda-helper-focused"]]: focused })}>
+            {text}
+        </label>
+    )
+}
