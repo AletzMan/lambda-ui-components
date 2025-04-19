5a7f9b77197bacca0dd3f3889aaa03bd71c5e30c
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-04-19 16:47:22 -0600

    feat: Update eslint.config.js to warn about unused variables with specific ignore patterns

--- eslint.config.js
+++ eslint.config.js
@@ -23,6 +23,14 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      "@typescript-eslint/no-unused-vars": [
+        "warn", // o "error"
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+        },
+      ],
     },
   },
 )
