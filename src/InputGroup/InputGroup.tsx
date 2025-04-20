8692c655c22a15436627584325b46b6d66ab9de4
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-04-20 13:36:39 -0600

    style: Refactor InputGroup component to use class-variance-authority styling

--- src/InputGroup/InputGroup.tsx
+++ src/InputGroup/InputGroup.tsx
@@ -3,52 +3,10 @@ import { ReactNode, forwardRef, createContext, useMemo, PropsWithChildren, FC, u
 import { InputProps } from "../Input/Input"
 import styles from "./inputGroup.module.css"
 import clsx from 'clsx'
-import { VariantProps, cva } from "class-variance-authority"
+import { VariantProps } from "class-variance-authority"
 import { InvalidMessage } from "../_util/InvalidMessage/InvalidMessage"
+import { inputGroup } from "./inputgroup.variants"
 
-export const inputGroup = cva(styles["lambda-input-group"], {
-    variants: {
-        size: {
-            tiny: styles["lambda-input-group-tiny"],
-            small: styles["lambda-input-group-small"],
-            medium: styles["lambda-input-group-medium"],
-            large: styles["lambda-input-group-large"],
-        },
-        variant: {
-            outline: styles["lambda-input-group-outline"],
-            flat: styles["lambda-input-group-flat"],
-            underline: styles["lambda-input-group-underline"],
-        },
-        radius: {
-            none: styles["lambda-input-group-radius-none"],
-            small: styles["lambda-input-group-radius-small"],
-            medium: styles["lambda-input-group-radius-medium"],
-            large: styles["lambda-input-group-radius-large"],
-            pill: styles["lambda-input-group-radius-pill"],
-        },
-        hasElements: {
-            none: styles["lambda-input-group-elements-none"],
-            first: styles["lambda-input-group-elements-first"],
-            last: styles["lambda-input-group-elements-last"],
-            both: styles["lambda-input-group-elements-both"]
-        },
-        invalid: {
-            true: styles["lambda-input-group-invalid"],
-            false: "",
-        },
-        disabled: {
-            false: styles["lambda-input-group-enabled"],
-            true: styles["lambda-input-group-disabled"],
-        },
-    },
-    defaultVariants: {
-        variant: "outline",
-        size: "medium",
-        radius: "medium",
-        invalid: false,
-        disabled: false,
-    },
-})
 
 
 type InputGroupContextType = {
