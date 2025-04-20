fff63a127fd96a1fcc8af1bf8fdf1ee62d1ea300
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-04-20 13:36:11 -0600

    chore: Refactor Button component to remove unused code and improve code organization

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,66 +1,12 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { ButtonHTMLAttributes, ReactNode, forwardRef, isValidElement } from "react"
 import styles from "./button.module.css"
-import { cva, VariantProps } from "class-variance-authority"
+import { VariantProps } from "class-variance-authority"
 import { Loader } from "lucide-react"
 import clsx from 'clsx'
 import { useInputGroup } from "../InputGroup/InputGroup"
+import { button } from "./button.variants"
 
-const button = cva(styles[`lambda-btn`], {
-    variants: {
-        color: {
-            primary: styles[`lambda-btn-primary`],
-            secondary: styles[`lambda-btn-secondary`],
-            danger: styles[`lambda-btn-danger`],
-            success: styles[`lambda-btn-success`],
-            warning: styles[`lambda-btn-warning`],
-            info: styles[`lambda-btn-info`],
-        },
-        variant: {
-            classic: styles[`lambda-btn-classic`],
-            solid: styles[`lambda-btn-solid`],
-            outline: styles[`lambda-btn-outline`],
-            dashed: styles[`lambda-btn-dashed`],
-            ghost: styles[`lambda-btn-ghost`],
-            text: styles[`lambda-btn-text`],
-        },
-        size: {
-            tiny: styles[`lambda-btn-tiny`],
-            small: styles[`lambda-btn-small`],
-            medium: styles[`lambda-btn-medium`],
-            large: styles[`lambda-btn-large`],
-        },
-        radius: {
-            none: styles[`lambda-btn-radius-none`],
-            small: styles[`lambda-btn-radius-small`],
-            medium: styles[`lambda-btn-radius-medium`],
-            large: styles[`lambda-btn-radius-large`],
-            pill: styles[`lambda-btn-radius-pill`],
-            circle: styles[`lambda-btn-radius-circle`],
-        },
-        iconPosition: {
-            left: styles[`lambda-btn-icon-left`],
-            right: styles[`lambda-btn-icon-right`],
-        },
-        loading: {
-            true: styles[`lambda-btn-loading`],
-            false: styles[`lambda-btn-normal`],
-        },
-        disabled: {
-            false: styles[`lambda-btn-enabled`],
-            true: styles[`lambda-btn-disabled`],
-        },
-    },
-    defaultVariants: {
-        color: "primary",
-        variant: "solid",
-        size: "medium",
-        radius: 'medium',
-        disabled: false,
-        iconPosition: "left",
-        loading: false,
-    },
-})
+
 
 export interface ButtonProps extends Omit<ButtonHTMLAttributes<HTMLButtonElement>, "disabled" | "color">, VariantProps<typeof button> {
     icon?: ReactNode | undefined | null
@@ -91,7 +37,7 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
             const context = useInputGroup()
             contextSize = context.size
             contextDisabled = context.disabled
-        } catch (e) {
+        } catch (_e) {
             contextSize = size
             contextDisabled = disabled
         }
