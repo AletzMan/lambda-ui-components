6f09fb692155480b464f652ecc05f82f23d8f9f3
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-04-24 00:30:38 -0600

    chore: Add Input component with variants and props

---
+++ src/components/Input/input.variants.ts
@@ -0,0 +1,154 @@
+import styles from "./input.module.css";
+import { VariantProps, cva, } from "class-variance-authority";
+
+export const lambdaInput = cva(styles["lambda-input"], {
+    variants: {
+        size: {
+            tiny: styles["lambda-input-tiny"],
+            small: styles["lambda-input-small"],
+            medium: styles["lambda-input-medium"],
+            large: styles["lambda-input-large"],
+        },
+        radius: {
+            none: styles["lambda-input-radius-none"],
+            small: styles["lambda-input-radius-small"],
+            medium: styles["lambda-input-radius-medium"],
+            large: styles["lambda-input-radius-large"],
+            pill: styles["lambda-input-radius-pill"],
+        },
+        disabled: {
+            false: styles["lambda-input-enabled"],
+            true: styles["lambda-input-disabled"],
+        },
+        invalid: {
+            true: styles["lambda-input-invalid"],
+            false: "",
+        },
+    },
+    defaultVariants: {
+        radius: "small",
+        disabled: false,
+    },
+});
+export const input = cva(styles["lambda-input-wrapper"], {
+    variants: {
+        size: {
+            tiny: styles["lambda-input-wrapper-tiny"],
+            small: styles["lambda-input-wrapper-small"],
+            medium: styles["lambda-input-wrapper-medium"],
+            large: styles["lambda-input-wrapper-large"],
+        },
+        variant: {
+            outline: styles["lambda-input-wrapper-outline"],
+            flat: styles["lambda-input-wrapper-flat"],
+            underline: styles["lambda-input-wrapper-underline"],
+        },
+        type: {
+            text: styles["lambda-input-wrapper-text"],
+            search: styles["lambda-input-wrapper-search"],
+            password: styles["lambda-input-wrapper-password"],
+            email: styles["lambda-input-wrapper-email"],
+        },
+        radius: {
+            none: styles["lambda-input-wrapper-radius-none"],
+            small: styles["lambda-input-wrapper-radius-small"],
+            medium: styles["lambda-input-wrapper-radius-medium"],
+            large: styles["lambda-input-wrapper-radius-large"],
+            pill: styles["lambda-input-wrapper-radius-pill"],
+        },
+        invalid: {
+            true: styles["lambda-input-wrapper-invalid"],
+            false: "",
+        },
+        hasElements: {
+            none: styles["lambda-input-wrapper-elements-none"],
+            first: styles["lambda-input-wrapper-elements-first"],
+            last: styles["lambda-input-wrapper-elements-last"],
+            both: styles["lambda-input-wrapper-elements-both"]
+        },
+        disabled: {
+            false: styles["lambda-input-wrapper-enabled"],
+            true: styles["lambda-input-wrapper-disabled"],
+        },
+    },
+    defaultVariants: {
+        variant: "outline",
+        size: "medium",
+        radius: "small",
+        type: "text",
+        hasElements: "none",
+        invalid: false,
+        disabled: false,
+    },
+});
+
+export const labels = cva(styles["lambda-input-label"], {
+    variants: {
+        size: {
+            tiny: styles["lambda-input-label-tiny"],
+            small: styles["lambda-input-label-small"],
+            medium: styles["lambda-input-label-medium"],
+            large: styles["lambda-input-label-large"],
+        },
+        radius: {
+            none: styles["lambda-input-label-radius-none"],
+            small: styles["lambda-input-label-radius-small"],
+            medium: styles["lambda-input-label-radius-medium"],
+            large: styles["lambda-input-label-radius-large"],
+            pill: styles["lambda-input-label-radius-pill"],
+        },
+        hasElements: {
+            none: styles["lambda-input-label-elements-none"],
+            first: styles["lambda-input-label-elements-first"],
+            last: styles["lambda-input-label-elements-last"],
+            both: styles["lambda-input-label-elements-both"]
+        },
+
+    },
+    defaultVariants: {
+        radius: "small",
+        size: "medium",
+    },
+});
+
+export const textInput = cva(styles["lambda-input-field"], {
+    variants: {
+        size: {
+            tiny: styles["lambda-input-field-tiny"],
+            small: styles["lambda-input-field-small"],
+            medium: styles["lambda-input-field-medium"],
+            large: styles["lambda-input-field-large"],
+        },
+        disabled: {
+            false: styles["lambda-input-field-disabled"],
+            true: styles["lambda-input-field-enabled"],
+        },
+    },
+    defaultVariants: {
+        size: "medium",
+        disabled: false
+    }
+});
+
+
+export const buttonPassword = cva(styles["lambda-input-toggle-password"], {
+    variants: {
+        size: {
+            tiny: styles["lambda-input-toggle-password-tiny"],
+            small: styles["lambda-input-toggle-password-small"],
+            medium: styles["lambda-input-toggle-password-medium"],
+            large: styles["lambda-input-toggle-password-large"],
+        },
+        variant: {
+            outline: styles["lambda-input-toggle-password-outline"],
+            flat: styles["lambda-input-toggle-password-flat"],
+            underline: styles["lambda-input-toggle-password-underline"],
+        },
+    },
+    defaultVariants: {
+        size: "medium",
+        variant: "outline"
+    }
+});
+
+export type InputVariants = VariantProps<typeof input> 
