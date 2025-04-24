300b3e448e594b19a064e0d5d9297e3dc1517b6c
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-04-24 00:10:38 -0600

    feat: Add Checkbox component with variants and props

---
+++ src/components/Checkbox/Checkbox.tsx
@@ -0,0 +1,64 @@
+import { forwardRef, useState, InputHTMLAttributes, ChangeEvent } from "react";
+import styles from "./checkbox.module.css";
+import { CheckIcon } from "lucide-react";
+import { CheckboxVariants, ContainerVariants, checkboxprop, container, icon, textLabel } from "./checkbox.variants";
+
+export interface CheckBoxProps
+    extends Omit<InputHTMLAttributes<HTMLInputElement>, "size" | "disabled" | 'checked' | "color" | "type" | "onChange"> {
+    size?: CheckboxVariants["size"];
+    variant?: CheckboxVariants["variant"];
+    radius?: CheckboxVariants["radius"];
+    color?: CheckboxVariants["color"];
+    positionLabel?: ContainerVariants["positionLabel"];
+    label?: string;
+    checked?: boolean;
+    onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+    disabled?: boolean;
+}
+
+export const Checkbox = forwardRef<HTMLInputElement, CheckBoxProps>(
+    (
+        {
+            className,
+            size,
+            variant,
+            label = "Label",
+            disabled,
+            radius,
+            positionLabel = "right",
+            color,
+            checked,
+            onChange,
+            ...props
+        },
+        ref
+    ) => {
+        const [internalChecked, setInternalChecked] = useState(checked);
+
+        const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+            const newChecked = e.currentTarget.checked;
+            setInternalChecked(newChecked);
+            if (onChange) {
+                onChange(e);
+            }
+        };
+
+        return (
+            <label className={container({ positionLabel, disabled })}>
+                <div className={checkboxprop({ variant, size, radius, color, checked: internalChecked, disabled, className })}>
+                    <input
+                        ref={ref}
+                        type={"checkbox"}
+                        disabled={disabled || undefined}
+                        checked={internalChecked}
+                        onChange={handleChange}
+                        className={styles["lambda-checkbox"]}
+                        {...props}
+                    />
+                    {<CheckIcon className={icon({ size, disabled, checked: internalChecked })} />}
+                </div>
+                {label && <span className={textLabel({ size, disabled })}>{label}</span>}
+            </label>
+        );
+    }
+);
---
+++ src/components/Checkbox/checkbox.stories.tsx
@@ -0,0 +1,116 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Checkbox } from "./Checkbox";
+
+const meta: Meta<typeof Checkbox> = {
+    title: "Components/Checkbox",
+    tags: ['autodocs'],
+    parameters: {
+        layout: "centered",
+    },
+    component: Checkbox,
+    argTypes: {
+        variant: {
+            control: "select",
+            options: ['solid', 'flat', 'outline'],
+            description: "Visual style of the input",
+            type: 'string',
+        },
+        color: {
+            control: "select",
+            options: ['primary', 'secondary', 'danger', 'success', 'warning', 'info'],
+            description: "Background color"
+        },
+        size: {
+            control: "select",
+            options: ['tiny', 'small', 'medium', 'large'],
+            description: "Input size"
+        },
+        radius: {
+            control: 'select',
+            options: ['none', 'small', 'medium', 'circle'],
+            description: "Corner shape",
+            type: "string"
+        },
+        label: {
+            control: 'text',
+            type: "string",
+            description: "Text to display as the label for the component",
+        },
+        positionLabel: {
+            control: 'select',
+            options: ['right', 'left', 'top', 'bottom'],
+            description: "Select position label right or left",
+            type: "string"
+        },
+        disabled: {
+            control: 'boolean',
+            description: "Disables the input and makes it inactive"
+        }
+    }
+
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Checkbox>
+
+export const Primary: Story = {
+    args: {
+        variant: "solid",
+        color: "primary",
+        size: 'medium',
+        label: 'Label',
+        positionLabel: 'right',
+        radius: "medium",
+        checked: false,
+        disabled: false,
+    },
+};
+export const Secondary: Story = {
+    args: {
+        variant: "solid",
+        color: "secondary",
+        size: 'medium',
+        label: 'Label',
+        positionLabel: 'right',
+        radius: "medium",
+        checked: false,
+        disabled: false,
+    },
+};
+export const Danger: Story = {
+    args: {
+        variant: "solid",
+        color: "danger",
+        size: 'medium',
+        label: 'Label',
+        positionLabel: 'right',
+        radius: "medium",
+        checked: false,
+        disabled: false,
+    },
+};
+export const Success: Story = {
+    args: {
+        variant: "solid",
+        color: "success",
+        size: 'medium',
+        label: 'Label',
+        positionLabel: 'right',
+        radius: "medium",
+        checked: false,
+        disabled: false,
+    },
+};
+export const Warning: Story = {
+    args: {
+        variant: "solid",
+        color: "warning",
+        size: 'medium',
+        label: 'Label',
+        positionLabel: 'right',
+        radius: "medium",
+        checked: false,
+        disabled: false,
+    },
+};
