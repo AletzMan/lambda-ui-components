1574f7d195493727250cd704062ceda87b4804d8
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-04-24 00:32:20 -0600

    feat: Add ButtonThemeController component for managing theme switching

---
+++ src/components/ThemeProvider/ButtonThemeController.tsx
@@ -0,0 +1,17 @@
+import { MoonIcon, SunIcon } from "lucide-react";
+import { Button } from "../Button/Button";
+import { useTheme } from "./ThemeProvider";
+
+export const ButtonThemeController = () => {
+    const { toggleTheme, theme } = useTheme();
+    const label = theme === "dark" ? "Switch to light theme" : "Switch to dark theme";
+    return (
+        <Button
+            color="secondary"
+            variant="ghost"
+            icon={theme === "dark" ? <MoonIcon /> : <SunIcon />}
+            onClick={toggleTheme}
+            aria-label={label}
+        />
+    );
+};
