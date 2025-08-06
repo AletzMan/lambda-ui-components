14b58ef85ad44c4e3d234f595df0caacb7e43651
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-08-06 00:22:07 -0600

    feat: add Storybook stories for Breadcrumb component

---
+++ src/components/Breadcrumb/breadcrumb.stories.tsx
@@ -0,0 +1,43 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { Breadcrumb } from "./Breadcrumb";
+
+const meta: Meta<typeof Breadcrumb> = {
+	title: "Components/Breadcrumb",
+	tags: ["autodocs"],
+	parameters: {
+		layout: "centered",
+	},
+	component: Breadcrumb,
+	argTypes: {
+		size: {
+			control: "select",
+			options: ["tiny", "small", "medium", "large"],
+			description: "Size of the breadcrumb",
+			type: "string",
+		},
+		variant: {
+			control: "select",
+			options: ["outline", "flat", "solid"],
+			description: "Variant of the breadcrumb",
+			type: "string",
+		},
+		separator: {
+			control: "select",
+			options: ["chevron", "slash", "dot", "arrow"],
+			description: "Separator of the breadcrumb",
+			type: "string",
+		},
+	},
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Breadcrumb>;
+
+export const Default: Story = {
+	args: {
+		size: "medium",
+		variant: "solid",
+		separator: "chevron",
+	},
+};
