5d5a66841c7a89824e13554d798d74a1bc866eb1
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-08-07 15:16:17 -0600

    refactor: change and add styles new props

--- src/components/Breadcrumb/Breadcrumb.tsx
+++ src/components/Breadcrumb/Breadcrumb.tsx
@@ -1,8 +1,9 @@
-import { forwardRef } from "react";
-import { BreadcrumbProps } from "./breadcrumb.types";
+import { forwardRef, useState, Fragment, useEffect } from "react";
+import { BreadcrumbProps, BreadcrumbItem as ItemBreadcrumb } from "./breadcrumb.types";
 import {
 	breadcrumb,
 	breadcrumbContainer,
+	breadcrumbEllipsis,
 	breadcrumbItem,
 	breadcrumbSeparator,
 } from "./breadcrumb.variants";
@@ -15,29 +16,81 @@ const NOTIFICATION_ICONS = {
 	arrow: <ArrowRight />,
 };
 
+const BreadcrumbItem = ({
+	item,
+	size,
+	variant,
+	color,
+}: {
+	item: ItemBreadcrumb;
+	size: "tiny" | "small" | "medium" | "large" | undefined;
+	variant: "none" | "flat" | "outline" | undefined;
+	color: "primary" | "secondary" | "success" | "danger" | "warning" | "info" | undefined;
+}) => {
+	const Component = item.href ? "a" : "span";
+	return (
+		<Component href={item.href} className={breadcrumbItem({ size, variant, color })}>
+			{item.icon}
+			{item.label}
+		</Component>
+	);
+};
+
 export const Breadcrumb = forwardRef<HTMLElement, BreadcrumbProps>(
-	({ items, size, variant, separator, color, ...props }, ref) => {
+	({ items, size, variant, separator, color, maxItems = 0, ...props }, ref) => {
+		const [collapsed, setCollapsed] = useState(maxItems !== 0);
 		const IconSeparator = NOTIFICATION_ICONS[separator as keyof typeof NOTIFICATION_ICONS];
+		const shouldCollapse = items.length > maxItems + 1;
+		const language = document.documentElement.lang;
+		console.log(language);
+
+		useEffect(() => {
+			if (maxItems === 0) {
+				setCollapsed(false);
+			} else {
+				setCollapsed(true);
+			}
+		}, [maxItems]);
+
+		const visibleItems =
+			shouldCollapse && collapsed ? [...items.slice(0, maxItems), items[items.length - 1]] : items;
+
+		const handleEllipsisClick = () => {
+			setCollapsed(false);
+		};
+
 		return (
 			<nav ref={ref} {...props} className={breadcrumb({ size, variant })}>
-				{items.map((item, index) => (
-					<div key={index} className={breadcrumbContainer({ size, variant })}>
-						{item.href ? (
-							<a href={item.href} className={breadcrumbItem({ size, variant, color })}>
-								{item.icon}
-								{item.label}
-							</a>
-						) : (
-							<span className={breadcrumbItem({ size, variant, color })}>
-								{item.icon}
-								{item.label}
-							</span>
-						)}
-						{index < items.length - 1 && (
-							<span className={breadcrumbSeparator({ size, variant })}>{IconSeparator}</span>
-						)}
-					</div>
-				))}
+				<ol className={breadcrumbContainer({ size, variant })}>
+					{visibleItems.map((item, index, array) => (
+						<Fragment key={index}>
+							{shouldCollapse && collapsed && index === maxItems && (
+								<>
+									<ul>
+										<button
+											className={breadcrumbEllipsis({ size })}
+											onClick={handleEllipsisClick}
+											title={language === "es" ? "Mostrar más" : "Show more"}
+										>
+											ˑˑˑ
+										</button>
+									</ul>
+									<ul>
+										<span className={breadcrumbSeparator({ size, variant })}>{IconSeparator}</span>
+									</ul>
+								</>
+							)}
+							<ul>
+								<BreadcrumbItem item={item} size={size} variant={variant} color={color} />
+							</ul>
+							{index < array.length - 1 && (
+								<ul>
+									<span className={breadcrumbSeparator({ size, variant })}>{IconSeparator}</span>
+								</ul>
+							)}
+						</Fragment>
+					))}
+				</ol>
 			</nav>
 		);
 	}
