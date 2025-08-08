57b185a9e896134ce2115c37ef1d10c350750375
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-08-07 22:52:31 -0600

    refactor: change view of elements in bradcrumb item

--- src/components/Breadcrumb/Breadcrumb.tsx
+++ src/components/Breadcrumb/Breadcrumb.tsx
@@ -27,12 +27,16 @@ const BreadcrumbItem = ({
 	variant: "none" | "flat" | "outline" | undefined;
 	color: "primary" | "secondary" | "success" | "danger" | "warning" | "info" | undefined;
 }) => {
-	const Component = item.href ? "a" : "span";
-	return (
-		<Component href={item.href} className={breadcrumbItem({ size, variant, color })}>
+	return item.href ? (
+		<a href={item.href} className={breadcrumbItem({ size, variant, color })}>
 			{item.icon}
 			{item.label}
-		</Component>
+		</a>
+	) : (
+		<span className={breadcrumbItem({ size, variant, color })}>
+			{item.icon}
+			{item.label}
+		</span>
 	);
 };
 
@@ -40,7 +44,7 @@ export const Breadcrumb = forwardRef<HTMLElement, BreadcrumbProps>(
 	({ items, size, variant, separator, color, maxItems = 0, ...props }, ref) => {
 		const [collapsed, setCollapsed] = useState(maxItems !== 0);
 		const IconSeparator = NOTIFICATION_ICONS[separator as keyof typeof NOTIFICATION_ICONS];
-		const shouldCollapse = items.length > maxItems + 1;
+		const shouldCollapse = items!.length > maxItems + 1;
 		const language = document.documentElement.lang;
 
 		useEffect(() => {
@@ -52,7 +56,7 @@ export const Breadcrumb = forwardRef<HTMLElement, BreadcrumbProps>(
 		}, [maxItems]);
 
 		const visibleItems =
-			shouldCollapse && collapsed ? [...items.slice(0, maxItems), items[items.length - 1]] : items;
+			shouldCollapse && collapsed ? [...items.slice(0, maxItems), items[items!.length - 1]] : items;
 
 		const handleEllipsisClick = () => {
 			setCollapsed(false);
@@ -82,7 +86,7 @@ export const Breadcrumb = forwardRef<HTMLElement, BreadcrumbProps>(
 							<ul>
 								<BreadcrumbItem item={item} size={size} variant={variant} color={color} />
 							</ul>
-							{index < array.length - 1 && (
+							{index < array!.length - 1 && (
 								<ul>
 									<span className={breadcrumbSeparator({ size, variant })}>{IconSeparator}</span>
 								</ul>
