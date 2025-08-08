012db1c9dc81cbd271ac84498e17d01db655f163
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-08-07 23:55:37 -0600

    reactor: remove unsued console.log

--- src/components/Breadcrumb/breadcrumb.stories.tsx
+++ src/components/Breadcrumb/breadcrumb.stories.tsx
@@ -1,4 +1,4 @@
-import { Meta, StoryObj } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/react";
 import { Breadcrumb } from "./Breadcrumb";
 
 const meta: Meta<typeof Breadcrumb> = {
