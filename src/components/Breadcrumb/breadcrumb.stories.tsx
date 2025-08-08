101f4df939112758178efadc066c729c9b839e19
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-08-08 01:15:16 -0600

    * chore(breadcrumb.stories.tsx): add color and maxItems controls to Breadcrumb component in Storybook * feat(breadcrumb.stories.tsx): add Icon story with icons for Breadcrumb items

--- src/components/Breadcrumb/breadcrumb.stories.tsx
+++ src/components/Breadcrumb/breadcrumb.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { Breadcrumb } from "./Breadcrumb";
+import { BookIcon, DatabaseIcon, HomeIcon } from "lucide-react";
 
 const meta: Meta<typeof Breadcrumb> = {
 	title: "Components/Breadcrumb",
@@ -27,6 +28,17 @@ const meta: Meta<typeof Breadcrumb> = {
 			description: "Separator of the breadcrumb",
 			type: "string",
 		},
+		color: {
+			control: "select",
+			options: ["primary", "secondary", "success", "danger", "warning", "info"],
+			description: "Color of the breadcrumb",
+			type: "string",
+		},
+		maxItems: {
+			control: "number",
+			description: "Maximum number of items to show",
+			type: "number",
+		},
 	},
 };
 
@@ -36,8 +48,51 @@ type Story = StoryObj<typeof Breadcrumb>;
 
 export const Default: Story = {
 	args: {
+		items: [
+			{
+				label: "Home",
+				href: "/",
+			},
+			{
+				label: "Library",
+				href: "/library",
+			},
+			{
+				label: "Data",
+				href: "/data",
+			},
+		],
 		size: "medium",
 		variant: "none",
 		separator: "chevron",
+		color: "primary",
+		maxItems: 0,
+	},
+};
+
+export const Icon: Story = {
+	args: {
+		items: [
+			{
+				label: "Home",
+				href: "/",
+				icon: <HomeIcon />,
+			},
+			{
+				label: "Library",
+				href: "/library",
+				icon: <BookIcon />,
+			},
+			{
+				label: "Data",
+				href: "/data",
+				icon: <DatabaseIcon />,
+			},
+		],
+		size: "medium",
+		variant: "none",
+		separator: "chevron",
+		color: "primary",
+		maxItems: 0,
 	},
 };
