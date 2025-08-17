7557ea75fdedef1fe190ce3c9cd5c3f9a3542e60
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-08-17 15:57:09 -0600

    feat(colorpicker.types.ts): add ColorPickerProps interface with props for color picker component

---
+++ src/components/ColorPicker/colorpicker.types.ts
@@ -0,0 +1,30 @@
+export interface ColorPickerProps
+	extends Omit<
+		React.InputHTMLAttributes<HTMLInputElement>,
+		"size" | "disabled" | "checked" | "color" | "onChange"
+	> {
+	/**
+	 * Valor actual del color seleccionado.
+	 */
+	value?: string;
+	/**
+	 * Define el valor actual del color seleccionado.
+	 */
+	onChange?: (value: string) => void;
+	/**
+	 * Define el valor actual del color seleccionado.
+	 */
+	disabled?: boolean;
+	/**
+	 * Define el formato del color seleccionado.
+	 */
+	format?: "hex" | "rgb" | "rgba" | "hsl" | "hsla";
+	/**
+	 * Define el tamaño del color picker.
+	 */
+	size?: "tiny" | "small" | "medium" | "large";
+	/**
+	 * Define el estilo del color picker.
+	 */
+	variant?: "solid" | "flat" | "outline";
+}
