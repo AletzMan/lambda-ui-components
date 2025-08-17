0e09cbdfa2e3e919ba0b08d1ba3efcb8f713a97a
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-08-17 15:56:41 -0600

    feat(colorpicker.stories.tsx): add ColorPicker component story with solid variant, medium size, and not disabled

---
+++ src/components/ColorPicker/colorpicker.stories.tsx
@@ -0,0 +1,40 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { ColorPicker } from "./ColorPicker";
+
+const meta: Meta<typeof ColorPicker> = {
+	title: "Components/ColorPicker",
+	tags: ["autodocs"],
+	parameters: {
+		layout: "centered",
+	},
+	component: ColorPicker,
+	argTypes: {
+		variant: {
+			control: "select",
+			options: ["solid", "flat", "outline"],
+			description: "Visual style of the input",
+			type: "string",
+		},
+		size: {
+			control: "select",
+			options: ["tiny", "small", "medium", "large"],
+			description: "Input size",
+		},
+		disabled: {
+			control: "boolean",
+			description: "Disables the input and makes it inactive",
+		},
+	},
+};
+
+export default meta;
+
+export type Story = StoryObj<typeof ColorPicker>;
+
+export const Solid: Story = {
+	args: {
+		variant: "solid",
+		size: "medium",
+		disabled: false,
+	},
+};
