fedaad900123c7b60b1b60f1c934f02e2eef8dce
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-08-20 00:33:15 -0600

    feat(Tag): implement Tag component with customizable styles and properties

---
+++ src/components/Tag/Tag.stories.tsx
@@ -0,0 +1,105 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { Tag } from "./Tag";
+import { BadgeCheck } from "lucide-react";
+
+const meta: Meta<typeof Tag> = {
+	title: "Components/Tag",
+	tags: ["autodocs"],
+	parameters: {
+		layout: "centered",
+	},
+	component: Tag,
+	argTypes: {
+		variant: {
+			control: "select",
+			options: ["solid", "outline", "flat", "dashed", "subtle"],
+			description: "Visual style of the input",
+			type: "string",
+		},
+		color: {
+			control: "select",
+			options: ["primary", "secondary", "success", "danger", "warning", "info"],
+			description: "Background color",
+		},
+		size: {
+			control: "select",
+			options: ["tiny", "small", "medium", "large"],
+			description: "Input size",
+		},
+		radius: {
+			control: "select",
+			options: ["none", "tiny", "small", "medium", "large", "full"],
+			description: "Input radius",
+		},
+		text: {
+			control: "text",
+			description: "Input text",
+		},
+		onClose: {
+			control: "boolean",
+			description: "Input onClose",
+		},
+	},
+};
+
+export default meta;
+
+export const Outline: StoryObj = {
+	args: {
+		variant: "outline",
+		color: "primary",
+		icon: <BadgeCheck />,
+		size: "small",
+		text: "Tag",
+		radius: "small",
+		onClose: false,
+	},
+};
+
+export const Solid: StoryObj = {
+	args: {
+		variant: "solid",
+		color: "primary",
+		icon: <BadgeCheck />,
+		size: "small",
+		text: "Tag",
+		radius: "small",
+		onClose: false,
+	},
+};
+
+export const Flat: StoryObj = {
+	args: {
+		variant: "flat",
+		color: "primary",
+		icon: <BadgeCheck />,
+		size: "small",
+		text: "Tag",
+		radius: "small",
+		onClose: false,
+	},
+};
+
+export const Dashed: StoryObj = {
+	args: {
+		variant: "dashed",
+		color: "primary",
+		icon: <BadgeCheck />,
+		size: "small",
+		text: "Tag",
+		radius: "small",
+		onClose: false,
+	},
+};
+
+export const Subtle: StoryObj = {
+	args: {
+		variant: "subtle",
+		color: "primary",
+		icon: <BadgeCheck />,
+		size: "small",
+		text: "Tag",
+		radius: "small",
+		onClose: false,
+	},
+};
---
+++ src/components/Tag/tag.types.ts
@@ -0,0 +1,31 @@
+export interface TagProps
+	extends Omit<React.HTMLAttributes<HTMLDivElement>, "size" | "disabled" | "checked" | "color"> {
+	/**
+	 * Define el estilo visual del tag, como la apariencia del carril y el pulgar.
+	 */
+	variant?: "outline" | "solid" | "flat" | "dashed" | "subtle" | undefined;
+	/**
+	 * Controla el tamaño visual del tag.
+	 */
+	size?: "tiny" | "small" | "medium" | "large" | undefined;
+	/**
+	 * Establece el esquema de color para el tag, típicamente afectando el color cuando está activado.
+	 */
+	color?: "primary" | "secondary" | "success" | "danger" | "warning" | "info" | undefined;
+	/**
+	 * Define la forma del tag (por ejemplo, con extremos redondeados o cuadrados).
+	 */
+	radius?: "none" | "tiny" | "small" | "medium" | "large" | "full" | undefined;
+	/**
+	 * Define el texto que se muestra en el tag.
+	 */
+	text?: string;
+	/**
+	 * Define el icono que se muestra en el tag.
+	 */
+	icon?: React.ReactNode;
+	/**
+	 * Define la funcionalidad de cierre del tag, si no se define no se muestra (no se muestra el icono de cierre).
+	 */
+	onClose?: () => void;
+}
