fafaf6c571c674f2f819c1f773d4a0ec3781d050
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-08-21 00:11:16 -0600

    feat(table.types.ts): add TableProps interface with size and variant properties

---
+++ src/components/Table/table.types.ts
@@ -0,0 +1,12 @@
+import { TableVariants } from "./table.variants";
+
+export interface TableProps extends Omit<React.HTMLAttributes<HTMLTableElement>, "size"> {
+	/**
+	 * Controla el tamaño visual de la tabla.
+	 */
+	size?: TableVariants["size"];
+	/**
+	 * Define el estilo visual de la tabla, como la apariencia de las filas y las celdas.
+	 */
+	variant?: TableVariants["variant"];
+}
