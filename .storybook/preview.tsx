33079a493e739d272abad611a828a8b29244f8d0
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-08-21 00:10:18 -0600

    refactor(Preview): standardize formatting and improve background color handling

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,56 +1,52 @@
 /* eslint-disable react-refresh/only-export-components */
-import '@fontsource-variable/plus-jakarta-sans';
+import "@fontsource-variable/plus-jakarta-sans";
 import React, { useEffect } from "react";
 import { ThemeProvider } from "../src/components/ThemeProvider/ThemeProvider";
 import { withThemeByClassName } from "@storybook/addon-themes";
 import type { Preview } from "@storybook/react";
-import '../src/index.css';
-
+import "../src/index.css";
 
 const ThemeDecorator = (Story, context) => {
-  const theme = context.globals.theme || "dark";
+	const theme = context.globals.theme || "dark";
 
+	useEffect(() => {
+		document.body.setAttribute("data-theme", theme || "dark");
+		document.body.style.backgroundColor = "var(--background-color)";
+		const element = document.getElementById("storybook-docs")?.children[0] as HTMLElement;
+		const container = document.querySelector(".docs-story") as HTMLElement;
+		if (element) {
+			element.style.backgroundColor = "transparent";
+		}
+		if (container) {
+			container.style.backgroundColor = "var(--surface-a)";
+		}
+	}, [theme, Story, context]);
 
-  useEffect(() => {
-    document.body.setAttribute("data-theme", theme || "dark");
-    document.body.style.backgroundColor = "var(--surface-b)";
-    const element = document.getElementById("storybook-docs")?.children[0] as HTMLElement;
-    const container = document.querySelector(".docs-story") as HTMLElement;
-    if (element) {
-      element.style.backgroundColor = "transparent";
-    }
-    if (container) {
-      container.style.backgroundColor = "var(--surface-b)";
-    }
-
-
-  }, [theme, Story, context]);
-
-  return (
-    <ThemeProvider defaultTheme={theme}>
-      <Story />
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider defaultTheme={theme}>
+			<Story />
+		</ThemeProvider>
+	);
 };
 
 const preview: Preview = {
-  parameters: {
-    options: {
-      storySort: {
-        method: "alphabetical",
-      },
-    },
-  },
-  decorators: [
-    withThemeByClassName({
-      defaultTheme: "dark",
-      themes: {
-        light: "light",
-        dark: "dark",
-      },
-    }),
-    ThemeDecorator, // Aquí pasas el componente directamente
-  ],
+	parameters: {
+		options: {
+			storySort: {
+				method: "alphabetical",
+			},
+		},
+	},
+	decorators: [
+		withThemeByClassName({
+			defaultTheme: "dark",
+			themes: {
+				light: "light",
+				dark: "dark",
+			},
+		}),
+		ThemeDecorator, // Aquí pasas el componente directamente
+	],
 };
 
 export default preview;
