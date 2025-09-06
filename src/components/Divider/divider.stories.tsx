759262984180aec5871bbefe587ba69b7e1bb215
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-09-05 19:40:27 -0600

    styles(divider): add styles to orientation and position text

--- src/components/Divider/divider.stories.tsx
+++ src/components/Divider/divider.stories.tsx
@@ -2,6 +2,9 @@ import { Meta, StoryObj } from "@storybook/react";
 import { Divider } from "./Divider";
 import { DividerProps } from "./divider.types";
 import { Card } from "../Card/Card";
+import { Button } from "../Button/Button";
+import { Switch } from "../Switch/Switch";
+import { useState } from "react";
 
 const meta: Meta<typeof Divider> = {
 	title: "Components/Divider",
@@ -28,16 +31,16 @@ const meta: Meta<typeof Divider> = {
 			options: ["primary", "secondary", "success", "danger", "warning", "info"],
 			description: "Background color",
 		},
-		size: {
+		contentPosition: {
 			control: "select",
-			options: ["xs", "sm", "md", "lg"],
-			description: "Size of the divider",
+			options: ["center", "start", "end"],
+			description: "Content position of the divider",
 			type: "string",
 		},
-		type: {
+		size: {
 			control: "select",
-			options: ["full", "inset", "between"],
-			description: "Type of the divider",
+			options: ["tiny", "small", "medium", "large"],
+			description: "Size of the divider",
 			type: "string",
 		},
 	},
@@ -48,24 +51,59 @@ export default meta;
 type Story = StoryObj<typeof Divider>;
 
 const Template = (args: DividerProps) => {
+	const [hasContent, setHasContent] = useState(false);
 	return (
-		<Card>
-			<div
-				style={{
-					height: "100px",
-					width: "100px",
-					display: "flex",
-					justifyContent: "center",
-					alignItems: "center",
-					flexDirection: args.orientation === "vertical" ? "row" : "column",
-					gap: "10px",
-				}}
-			>
-				<p>Text</p>
-				<Divider {...args} />
-				<p>Text</p>
-			</div>
-		</Card>
+		<div style={{ display: "flex", flexDirection: "column", alignItems: "center", gap: "20px" }}>
+			<Switch
+				label="Has Content"
+				checked={hasContent}
+				onChange={(e) => setHasContent(e.target.checked)}
+			/>
+			<Card>
+				<div
+					style={{
+						display: "flex",
+						justifyContent: "center",
+						padding: "10px",
+						height: "250px",
+						width: "250px",
+						alignItems: "center",
+						flexDirection: args.orientation === "vertical" ? "row" : "column",
+						gap: "10px",
+					}}
+				>
+					<p
+						style={{
+							display: "flex",
+							justifyContent: "center",
+							alignItems: "center",
+							backgroundColor: "var(--surface-c)",
+							padding: "10px",
+							width: "100%",
+							height: "100%",
+							borderRadius: "var(--border-radius-sm)",
+						}}
+					>
+						Content
+					</p>
+					<Divider {...args}>{hasContent ? "Label" : ""}</Divider>
+					<p
+						style={{
+							display: "flex",
+							justifyContent: "center",
+							alignItems: "center",
+							backgroundColor: "var(--surface-c)",
+							padding: "10px",
+							width: "100%",
+							height: "100%",
+							borderRadius: "var(--border-radius-sm)",
+						}}
+					>
+						Content
+					</p>
+				</div>
+			</Card>
+		</div>
 	);
 };
 
@@ -75,7 +113,7 @@ export const Default: Story = {
 		variant: "solid",
 		orientation: "horizontal",
 		color: "primary",
-		size: "xs",
-		type: "full",
+		size: "tiny",
+		contentPosition: "center",
 	},
 };
