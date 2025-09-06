f3eaee95ab96892156b998ce56f0ff68d4b77bcb
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-09-06 14:39:09 -0600

    refactor(accordio): add wrapper story component

--- src/components/Accordion/accordion.stories.tsx
+++ src/components/Accordion/accordion.stories.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import { AccordionItem } from "./AccordionItem";
 import { AccordionContent } from "./AccordionContent";
 import { AccordionHeader } from "./AccordionHeader";
+import ContainerComponent from "../../_util/storybook/components/ContainerComponent/ContainerComponent";
 
 const sizeAccordion: AccordionSize[] = ["tiny", "small", "medium", "large"];
 
@@ -60,54 +61,56 @@ const PreviewAccordion = (
 	const [openItem, setOpenItem] = useState<AccordionValue>("");
 
 	return (
-		<Accordion
-			value={openItem}
-			onValueChange={(value) => setOpenItem(value)}
-			{...args}
-			style={{ marginBottom: "30px" }}
-		>
-			<AccordionItem value="item-1">
-				<AccordionHeader>Section 1: Introduction</AccordionHeader>
-				<AccordionContent>
-					<p>Here goes the content of section 1.</p>
-					<p>It can be text, images, other components.</p>
-				</AccordionContent>
-			</AccordionItem>
+		<ContainerComponent title="Accordion" subtitle={args?.variant?.toString() || ""}>
+			<Accordion
+				value={openItem}
+				onValueChange={(value) => setOpenItem(value)}
+				{...args}
+				style={{ marginBottom: "30px" }}
+			>
+				<AccordionItem value="item-1">
+					<AccordionHeader>Section 1: Introduction</AccordionHeader>
+					<AccordionContent>
+						<p>Here goes the content of section 1.</p>
+						<p>It can be text, images, other components.</p>
+					</AccordionContent>
+				</AccordionItem>
 
-			<AccordionItem value="item-2" disabled>
-				<AccordionHeader>Section 2: Components</AccordionHeader>
-				<AccordionContent>
-					<ul>
-						<li>Alert Component</li>
-						<li>Button Component</li>
-						<li>Link Component</li>
-						<li>Accordion Component</li>
-					</ul>
-					<div style={{ height: "50px" }}>Extra content to test transition</div>
-				</AccordionContent>
-			</AccordionItem>
+				<AccordionItem value="item-2" disabled>
+					<AccordionHeader>Section 2: Components</AccordionHeader>
+					<AccordionContent>
+						<ul>
+							<li>Alert Component</li>
+							<li>Button Component</li>
+							<li>Link Component</li>
+							<li>Accordion Component</li>
+						</ul>
+						<div style={{ height: "50px" }}>Extra content to test transition</div>
+					</AccordionContent>
+				</AccordionItem>
 
-			<AccordionItem value="item-3">
-				{" "}
-				{/* Disabled Item */}
-				<AccordionHeader>Section 3: Disabled</AccordionHeader>
-				<AccordionContent>
-					This content cannot be expanded because the item is disabled.
-				</AccordionContent>
-			</AccordionItem>
+				<AccordionItem value="item-3">
+					{" "}
+					{/* Disabled Item */}
+					<AccordionHeader>Section 3: Disabled</AccordionHeader>
+					<AccordionContent>
+						This content cannot be expanded because the item is disabled.
+					</AccordionContent>
+				</AccordionItem>
 
-			<AccordionItem value="item-4">
-				<AccordionHeader>Section 4: With Long Content</AccordionHeader>
-				<AccordionContent>
-					<p>
-						This is the beginning of content that is a bit longer to test scrolling if the
-						Accordion's main container allows it.
-					</p>
-					<div style={{ height: "400px", background: "#e9e9e9" }}></div>
-					<p>End of long content.</p>
-				</AccordionContent>
-			</AccordionItem>
-		</Accordion>
+				<AccordionItem value="item-4">
+					<AccordionHeader>Section 4: With Long Content</AccordionHeader>
+					<AccordionContent>
+						<p>
+							This is the beginning of content that is a bit longer to test scrolling if the
+							Accordion's main container allows it.
+						</p>
+						<div style={{ height: "400px", background: "#e9e9e9" }}></div>
+						<p>End of long content.</p>
+					</AccordionContent>
+				</AccordionItem>
+			</Accordion>
+		</ContainerComponent>
 	);
 };
 
