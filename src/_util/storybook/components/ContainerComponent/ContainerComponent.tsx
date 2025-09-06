1715dee504fd5e64781caafed497b595d57948f8
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-09-06 16:00:45 -0600

    refactor(container stories): add variable colors

--- src/_util/storybook/components/ContainerComponent/ContainerComponent.tsx
+++ src/_util/storybook/components/ContainerComponent/ContainerComponent.tsx
@@ -3,15 +3,39 @@ import styles from "./container.module.css";
 
 interface ContainerComponentProps {
 	title: string;
-	subtitle: string;
+	subtitle?: string;
+	color?: string;
 	children: React.ReactNode;
 }
 
-export default function ContainerComponent({ title, subtitle, children }: ContainerComponentProps) {
+const colors = {
+	default: "var(--secondary-text-color)",
+	primary: "var(--primary-text-color)",
+	danger: "var(--danger-text-color)",
+	success: "var(--success-text-color)",
+	warning: "var(--warning-text-color)",
+	info: "var(--info-text-color)",
+};
+
+export default function ContainerComponent({
+	title,
+	subtitle,
+	color,
+	children,
+}: ContainerComponentProps) {
+	const colorValue = colors[color as keyof typeof colors] || colors.default;
+	console.log(colorValue);
 	return (
 		<div className={styles.container}>
 			<h1 className={styles.title}>{title}</h1>
-			<h2 className={styles.subtitle}>{subtitle}</h2>
+			<div className={styles.subtitleContainer}>
+				{subtitle && <h2 className={styles.subtitle}>{subtitle}</h2>}
+				{color && (
+					<span className={styles.color} style={{ color: colorValue }}>
+						{color}
+					</span>
+				)}
+			</div>
 			<Divider color="default" />
 			<div className={styles.content}>{children}</div>
 		</div>
