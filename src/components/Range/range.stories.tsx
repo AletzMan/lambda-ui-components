952f05c9c175ebd43770472788ce6e16bec7d537
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-09-09 23:39:42 -0600

    refactor(range): update container styles for improved layout and spacing

--- src/components/Range/range.stories.tsx
+++ src/components/Range/range.stories.tsx
@@ -2,13 +2,11 @@ import type { Meta, StoryObj } from "@storybook/react";
 import { Range } from "./Range";
 import { useState } from "react";
 import { RangeProps, RangeValue } from "./range.types";
+import ContainerComponent from "../../_util/storybook/components/ContainerComponent/ContainerComponent";
+import { Input } from "../Input/Input";
 
 const meta: Meta<typeof Range> = {
 	title: "Components/Range",
-	tags: ["autodocs"],
-	parameters: {
-		layout: "centered",
-	},
 	component: Range,
 	argTypes: {
 		onChange: {
@@ -71,22 +69,77 @@ const meta: Meta<typeof Range> = {
 
 export default meta;
 
-const RangeWithControls = (args: RangeProps & React.RefAttributes<HTMLDivElement>) => {
-	const [valueRange, setValueRange] = useState<RangeValue>([10, 80]);
+const RangeSingle = (args: RangeProps) => {
+	const [valueRange, setValueRange] = useState<RangeValue>(50);
 	return (
-		<div style={{ width: "300px", padding: "20px" }}>
-			<Range
-				{...args}
-				value={valueRange}
-				onInput={(e) => setValueRange(e)}
-				onChange={(e) => setValueRange(e)}
-			/>
-		</div>
+		<ContainerComponent title="Range" subtitle="Single">
+			<div style={{ width: "130px", padding: "20px" }}>
+				<Input
+					value={valueRange.toString()}
+					disabled
+					style={{ textAlign: "center" }}
+					label="Value"
+				/>
+			</div>
+			<div style={{ width: "100%", maxWidth: "700px", padding: "20px" }}>
+				<Range
+					{...args}
+					value={valueRange}
+					onInput={(e) => setValueRange(e)}
+					onChange={(e) => setValueRange(e)}
+				/>
+			</div>
+		</ContainerComponent>
 	);
 };
 
-export const Default: StoryObj<typeof Range> = {
-	render: (args) => <RangeWithControls {...args} />,
+const RangeDouble = (args: RangeProps & React.RefAttributes<HTMLDivElement>) => {
+	const [valueRange, setValueRange] = useState<RangeValue>([10, 80]);
+	return (
+		<ContainerComponent title="Range" subtitle="Double">
+			<div style={{ display: "flex", gap: "var(--gap-lg)", width: "150px", padding: "20px" }}>
+				<Input
+					value={valueRange.toString().split(",")[0]}
+					disabled
+					style={{ textAlign: "center" }}
+					label="Min"
+				/>
+				<Input
+					value={valueRange.toString().split(",")[1]}
+					disabled
+					style={{ textAlign: "center" }}
+					label="Max"
+				/>
+			</div>
+			<div style={{ width: "100%", maxWidth: "700px", padding: "20px" }}>
+				<Range
+					{...args}
+					value={valueRange}
+					onInput={(e) => setValueRange(e)}
+					onChange={(e) => setValueRange(e)}
+				/>
+			</div>
+		</ContainerComponent>
+	);
+};
+
+export const Single: StoryObj<typeof Range> = {
+	render: (args) => <RangeSingle {...args} />,
+	args: {
+		min: 0,
+		max: 100,
+		step: 1,
+		disabled: false,
+		size: "medium",
+		label: "",
+		ariaLabel: "",
+		viewValue: true,
+		viewBar: true,
+	},
+};
+
+export const Double: StoryObj<typeof Range> = {
+	render: (args) => <RangeDouble {...args} />,
 	args: {
 		min: 0,
 		max: 100,
