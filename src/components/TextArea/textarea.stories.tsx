945721220b6eb49aeef405213b216dfd49455603
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-09-13 00:31:33 -0600

    feat: add TextArea stories and CSS modules for form components

--- src/components/TextArea/textarea.stories.tsx
+++ src/components/TextArea/textarea.stories.tsx
@@ -1,52 +1,76 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { TextArea } from "./TextArea";
+import { TextAreaProps } from "./textarea.types";
+import ContainerComponent from "../../_util/storybook/components/ContainerComponent/ContainerComponent";
 
 const meta: Meta<typeof TextArea> = {
-    title: "Components/TextArea",
-    tags: ['autodocs'],
-    parameters: {
-        layout: "centered",
-    },
-    component: TextArea,
-    argTypes: {
-        variant: {
-            control: "select",
-            options: ['borderless ', 'outline'],
-            description: "Optional variant of the card",
-            type: 'string',
-        },
-        size: {
-            control: "select",
-            options: ['tiny', 'small', 'medium', 'large'],
-            description: "Size of the card",
-        },
-        invalid: {
-            control: "boolean",
-            description: "Invalid state of the card",
-        },
-        disabled: {
-            control: "boolean",
-            description: "Disabled state of the card",
-        },
-        radius: {
-            control: 'select',
-            options: ['none', 'small', 'medium', 'large'],
-            description: "Radius of the card",
-        }
-    }
-
+	title: "Components/TextArea",
+	component: TextArea,
+	argTypes: {
+		variant: {
+			table: {
+				disabled: true,
+			},
+		},
+		size: {
+			control: "select",
+			options: ["tiny", "small", "medium", "large"],
+			description: "Size of the card",
+		},
+		invalid: {
+			control: "boolean",
+			description: "Invalid state of the card",
+		},
+		disabled: {
+			control: "boolean",
+			description: "Disabled state of the card",
+		},
+		radius: {
+			control: "select",
+			options: ["none", "small", "medium", "large"],
+			description: "Radius of the card",
+		},
+	},
 };
 
 export default meta;
 
-type Story = StoryObj<typeof TextArea>
+type Story = StoryObj<typeof TextArea>;
 
-export const Outline: Story = {
-    args: {
-        variant: "outline",
-        size: "medium",
-        radius: 'small',
-    },
+const Template = (args: TextAreaProps) => {
+	return (
+		<ContainerComponent
+			title="TextArea"
+			subtitle={args.variant || ""}
+			color={args.color?.toString() || ""}
+		>
+			<TextArea {...args} />
+		</ContainerComponent>
+	);
 };
 
+export const Outline: Story = {
+	render: Template,
+	args: {
+		variant: "outline",
+		size: "medium",
+		radius: "small",
+		invalid: false,
+		disabled: false,
+		label: "Outline",
+		placeholder: "Placeholder",
+	},
+};
 
+export const Soft: Story = {
+	render: Template,
+	args: {
+		variant: "soft",
+		size: "medium",
+		radius: "small",
+		invalid: false,
+		disabled: false,
+		label: "Soft",
+		placeholder: "Placeholder",
+	},
+};
