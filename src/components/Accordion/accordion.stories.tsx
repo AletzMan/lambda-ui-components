b68c508b0223a431432e424092efa1850c97f3ef
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-09-13 02:05:27 -0600

    feat: add accordion component with styles and storybook examples

--- src/components/Accordion/accordion.stories.tsx
+++ src/components/Accordion/accordion.stories.tsx
@@ -62,54 +62,55 @@ const PreviewAccordion = (
 
 	return (
 		<ContainerComponent title="Accordion" subtitle={args?.variant?.toString() || ""}>
-			<Accordion
-				value={openItem}
-				onValueChange={(value) => setOpenItem(value)}
-				{...args}
-				style={{ marginBottom: "30px" }}
-			>
-				<AccordionItem value="item-1">
-					<AccordionHeader>Section 1: Introduction</AccordionHeader>
-					<AccordionContent>
-						<p>Here goes the content of section 1.</p>
-						<p>It can be text, images, other components.</p>
-					</AccordionContent>
-				</AccordionItem>
+			<div style={{ padding: "10px" }}>
+				<Accordion
+					value={openItem}
+					onValueChange={(value) => setOpenItem(value)}
+					{...args}
+					style={{ marginBottom: "30px" }}
+				>
+					<AccordionItem value="item-1">
+						<AccordionHeader>Section 1: Introduction</AccordionHeader>
+						<AccordionContent>
+							<p>Here goes the content of section 1.</p>
+							<p>It can be text, images, other components.</p>
+						</AccordionContent>
+					</AccordionItem>
 
-				<AccordionItem value="item-2" disabled>
-					<AccordionHeader>Section 2: Components</AccordionHeader>
-					<AccordionContent>
-						<ul>
-							<li>Alert Component</li>
-							<li>Button Component</li>
-							<li>Link Component</li>
-							<li>Accordion Component</li>
-						</ul>
-						<div style={{ height: "50px" }}>Extra content to test transition</div>
-					</AccordionContent>
-				</AccordionItem>
+					<AccordionItem value="item-2">
+						<AccordionHeader>Section 2: Components</AccordionHeader>
+						<AccordionContent>
+							<ul>
+								<li>Alert Component</li>
+								<li>Button Component</li>
+								<li>Link Component</li>
+								<li>Accordion Component</li>
+							</ul>
+							<div style={{ height: "50px" }}>Extra content to test transition</div>
+						</AccordionContent>
+					</AccordionItem>
 
-				<AccordionItem value="item-3">
-					{" "}
-					{/* Disabled Item */}
-					<AccordionHeader>Section 3: Disabled</AccordionHeader>
-					<AccordionContent>
-						This content cannot be expanded because the item is disabled.
-					</AccordionContent>
-				</AccordionItem>
+					<AccordionItem value="item-3" disabled>
+						{/* Disabled Item */}
+						<AccordionHeader>Section 3: Disabled</AccordionHeader>
+						<AccordionContent>
+							This content cannot be expanded because the item is disabled.
+						</AccordionContent>
+					</AccordionItem>
 
-				<AccordionItem value="item-4">
-					<AccordionHeader>Section 4: With Long Content</AccordionHeader>
-					<AccordionContent>
-						<p>
-							This is the beginning of content that is a bit longer to test scrolling if the
-							Accordion's main container allows it.
-						</p>
-						<div style={{ height: "400px", background: "#e9e9e9" }}></div>
-						<p>End of long content.</p>
-					</AccordionContent>
-				</AccordionItem>
-			</Accordion>
+					<AccordionItem value="item-4">
+						<AccordionHeader>Section 4: With Long Content</AccordionHeader>
+						<AccordionContent>
+							<p>
+								This is the beginning of content that is a bit longer to test scrolling if the
+								Accordion's main container allows it.
+							</p>
+							<div style={{ height: "400px", background: "#e9e9e9" }}></div>
+							<p>End of long content.</p>
+						</AccordionContent>
+					</AccordionItem>
+				</Accordion>
+			</div>
 		</ContainerComponent>
 	);
 };
