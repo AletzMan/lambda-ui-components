e62e677b0d97c5cfd08115c99b9785f9332be61d
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-09-19 02:14:00 -0600

    feat: add Storybook preview configuration with theme support and custom decorators

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -25,7 +25,7 @@ const ThemeDecorator = (Story, context) => {
 	}, [theme, Story, context]);
 
 	return (
-		<LambdaConfigProvider lang="en" radiusField="tiny" radiusBox="small">
+		<LambdaConfigProvider lang="en" radiusField="tiny" radiusBox="none">
 			<ThemeProvider defaultTheme={theme}>
 				<Story />
 			</ThemeProvider>
