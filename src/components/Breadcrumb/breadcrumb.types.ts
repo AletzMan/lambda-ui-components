4eefb0f77a7e194f69e26b27655177d94b404ca7
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-09-20 19:31:58 -0600

    feat(breadcrumb): define types and interfaces for breadcrumb component

--- src/components/Breadcrumb/breadcrumb.types.ts
+++ src/components/Breadcrumb/breadcrumb.types.ts
@@ -1,3 +1,5 @@
+import { BreadcrumbVariants } from "./breadcrumb.variants";
+
 export interface BreadcrumbProps {
 	/**
 	 * Array of breadcrumb items
@@ -6,15 +8,19 @@ export interface BreadcrumbProps {
 	/**
 	 * Size of the breadcrumb
 	 */
-	size?: "tiny" | "small" | "medium" | "large" | undefined;
+	size?: BreadcrumbVariants["size"];
+	/**
+	 * Radius of the breadcrumb
+	 */
+	radius?: BreadcrumbVariants["radius"];
 	/**
 	 * Variant of the breadcrumb separator
 	 */
-	variant?: "chevron" | "slash" | "dot" | "arrow" | "stepped" | undefined;
+	variant?: BreadcrumbVariants["variant"];
 	/**
 	 * Color of the breadcrumb
 	 */
-	color?: "primary" | "secondary" | "success" | "danger" | "warning" | "info" | undefined;
+	color?: BreadcrumbVariants["color"];
 	/**
 	 * Maximun number of items to show
 	 */
