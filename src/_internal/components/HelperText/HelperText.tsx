03c7c5c4fa3fb00eb124daee0537bd120648c948
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-09-25 23:40:23 -0600

    styles(helperText): remove floating translation

--- src/_internal/components/HelperText/HelperText.tsx
+++ src/_internal/components/HelperText/HelperText.tsx
@@ -3,37 +3,35 @@ import styles from "./styles.module.css";
 import clsx from "clsx";
 
 const helper = cva(styles["lambda-helper"], {
-    variants: {
-        size: {
-            tiny: styles["lambda-helper-tiny"],
-            small: styles["lambda-helper-small"],
-            medium: styles["lambda-helper-medium"],
-            large: styles["lambda-helper-large"],
-        },
-        disabled: {
-            false: styles["lambda-helper-enabled"],
-            true: styles["lambda-helper-disabled"],
-        },
-    },
-    defaultVariants: {
-        size: "medium",
-        disabled: false
-    }
+	variants: {
+		size: {
+			tiny: styles["lambda-helper-tiny"],
+			small: styles["lambda-helper-small"],
+			medium: styles["lambda-helper-medium"],
+			large: styles["lambda-helper-large"],
+		},
+		disabled: {
+			false: styles["lambda-helper-enabled"],
+			true: styles["lambda-helper-disabled"],
+		},
+	},
+	defaultVariants: {
+		size: "medium",
+		disabled: false,
+	},
 });
 
-
 interface HelperTextProps {
-    text: string
-    size?: "tiny" | "small" | "medium" | "large" | null
-    disabled?: boolean | null
-    focused?: boolean | null
-    id?: string | undefined
+	text: string;
+	size?: "tiny" | "small" | "medium" | "large" | null;
+	disabled?: boolean | null;
+	id?: string | undefined;
 }
 
-export function HelperText({ text, size = "medium", disabled = false, focused, id }: HelperTextProps) {
-    return (
-        <label className={clsx(helper({ disabled, size }), { [styles["lambda-helper-focused"]]: focused })} id={id}>
-            {text}
-        </label>
-    );
+export function HelperText({ text, size = "medium", disabled = false, id }: HelperTextProps) {
+	return (
+		<label className={clsx(helper({ disabled, size }))} id={id}>
+			{text}
+		</label>
+	);
 }
