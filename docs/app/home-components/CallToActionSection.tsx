98bcf820de0083ef46a47b814a840498e3180876
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-11-03 16:22:36 -0600

    feat(home): add call-to-action section with get started and GitHub buttons

---
+++ docs/app/home-components/CallToActionSection.tsx
@@ -0,0 +1,41 @@
+"use client";
+import { GitHubIcon } from "@/components/icons/GitHub";
+import { Link } from "lambda-ui-components";
+import { Rocket } from "lucide-react";
+
+export function CallToActionSection() {
+	return (
+		<section className="w-full flex flex-col items-center justify-center py-25 my-25">
+			<h2 className="text-3xl md:text-4xl font-bold text-center mb-4">
+				Ready to build your next masterpiece?
+			</h2>
+			<p className="text-lg text-(--foreground-secondary-color) text-center mb-8 max-w-xl">
+				Start using Lambda UI Components and create beautiful, modern interfaces in minutes.
+			</p>
+			<div className="flex gap-4">
+				<Link
+					label="Get Started"
+					size="large"
+					color="primary"
+					variant="solid"
+					radius="small"
+					href="/introduction"
+					target="_blank"
+					iconPosition="left"
+					icon={<Rocket />}
+					type="button"
+				/>
+				<Link
+					label="Star on GitHub"
+					size="large"
+					color="neutral"
+					variant="solid"
+					icon={<GitHubIcon />}
+					href="https://github.com/AletzMan/lambda-ui-components"
+					target="_blank"
+					type="button"
+				/>
+			</div>
+		</section>
+	);
+}
--- docs/app/page.tsx
+++ docs/app/page.tsx
@@ -1,12 +1,14 @@
 import { FeatureSection } from "./home-components/FeatureSection";
 import HomeHero from "./home-components/HomeHero";
 import { ExamplesSection } from "./home-components/ExamplesSection";
+import { CallToActionSection } from "./home-components/CallToActionSection";
 export default function Home() {
 	return (
-		<div className="flex min-h-screen flex-col text-(--foreground-color) mx-auto w-full">
+		<div className="flex flex-col text-(--foreground-color) mx-auto w-full">
 			<HomeHero />
 			<FeatureSection />
 			<ExamplesSection />
+			<CallToActionSection />
 		</div>
 	);
 }
