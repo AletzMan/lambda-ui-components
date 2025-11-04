00716621f3bf3844e474a85753f211fa903fa842
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-11-04 13:50:30 -0600

    refactor(home): move home components to /components and update imports
    Moved all home page components to the new /components directory and updated imports in page.tsx for better organization and maintainability.

--- docs/app/page.tsx
+++ docs/app/page.tsx
@@ -1,8 +1,8 @@
-import { FeatureSection } from "./home-components/FeatureSection";
-import HomeHero from "./home-components/HomeHero";
-import { ExamplesSection } from "./home-components/ExamplesSection";
-import { CallToActionSection } from "./home-components/CallToActionSection";
-import { HomeFooter } from "./home-components/HomeFooter";
+import { FeatureSection } from "./components/FeatureSection";
+import HomeHero from "./components/HomeHero";
+import { ExamplesSection } from "./components/ExamplesSection";
+import { CallToActionSection } from "./components/CallToActionSection";
+import { HomeFooter } from "./components/HomeFooter";
 export default function Home() {
 	return (
 		<div className="flex flex-col text-(--foreground-color) mx-auto w-full">
