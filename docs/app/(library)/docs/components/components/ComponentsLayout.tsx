fa72d17f354b28a1d64ee2ae247dc66bbb543b92
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-11-05 19:15:34 -0600

    feat(ComponentsLayout): enhance layout with tabs and navigation menu for features and API reference

--- docs/app/(library)/docs/components/components/ComponentsLayout.tsx
+++ docs/app/(library)/docs/components/components/ComponentsLayout.tsx
@@ -1,13 +1,20 @@
 "use client";
-import { NavigationMenu, NavigationMenuData, useActiveSectionObserver } from "lambda-ui-components";
+import {
+	NavigationMenu,
+	NavigationMenuData,
+	Tabs,
+	useActiveSectionObserver,
+} from "lambda-ui-components";
 import { BarNavButton } from "@/app/(library)/components/layout/BarNavButton";
 import { FooterDocs } from "@/components/layout/FooterDocs";
+import { useState } from "react";
+import { Features } from "./Features";
+import { List } from "lucide-react";
 
 interface ComponentsLayoutProps {
 	title?: string;
 	description?: string;
 	children: React.ReactNode;
-	data: NavigationMenuData[];
 	buttonLeft?: {
 		href: string;
 		text: string;
@@ -18,14 +25,60 @@ interface ComponentsLayoutProps {
 	};
 }
 
+const dataFeatures: NavigationMenuData[] = [
+	{
+		id: "on-this-page",
+		label: "On this page",
+		icon: <List />,
+		children: [
+			{
+				id: "playground",
+				label: "Playground",
+				path: "#playground",
+				target: "_top",
+			},
+			{
+				id: "usage",
+				label: "Usage",
+				path: "#usage",
+				target: "_top",
+			},
+			{
+				id: "variants",
+				label: "Variants",
+				path: "#variants",
+				target: "_top",
+			},
+			{
+				id: "colors",
+				label: "Colors",
+				path: "#colors",
+				target: "_top",
+			},
+			{
+				id: "sizes",
+				label: "Sizes",
+				path: "#sizes",
+				target: "_top",
+			},
+		],
+	},
+];
+const dataApiReference: NavigationMenuData[] = [
+	{
+		id: "api",
+		label: "",
+	},
+];
+
 export const ComponentsLayout = ({
 	children,
-	data,
 	title,
 	description,
 	buttonLeft,
 	buttonRight,
 }: ComponentsLayoutProps) => {
+	const [tabActive, setTabActive] = useState(0);
 	const activeId = useActiveSectionObserver({ selectors: "h2", rootMargin: "-72px 0px -70% 0px" });
 	return (
 		<section className="relative flex flex-col gap-10 px-6 py-4">
@@ -39,24 +92,43 @@ export const ComponentsLayout = ({
 					</p>
 				)}
 			</header>
-			<div className="grid grid-cols-[1fr_250px]">
-				<div>
-					<div className="bg-(--surface-a)/55 backdrop-blur-[2px] p-1 rounded-sm rounded-tr-none border border-(--border-color)/30">
-						{children}
-					</div>
-					<BarNavButton buttonLeft={buttonLeft} buttonRight={buttonRight} />
-					<FooterDocs />
+			<div className="flex flex-col gap-10">
+				<div className="sticky top-20 bg-(--surface-a)/55 backdrop-blur-[2px] p-1 ">
+					{children}
+					<Tabs
+						variant="underline"
+						size="large"
+						color="primary"
+						radius="small"
+						onChange={(index) => setTabActive(index)}
+					>
+						<Tabs.List>
+							<Tabs.Tab title="Features" />
+							<Tabs.Tab title="API Reference" />
+							<Tabs.Tab title="ChangeLog" />
+						</Tabs.List>
+						<Tabs.Panels>
+							<Tabs.Panel>
+								<div className="grid grid-cols-[1fr_250px]">
+									<Features />
+									<aside className="sticky top-18 h-[calc(100vh-90px)] bg-(--background-color)">
+										<NavigationMenu
+											data={dataFeatures}
+											currentPath={"#" + activeId || ""}
+											alwaysOpen
+											showLines
+											styleLines="dotted"
+										/>
+									</aside>
+								</div>
+							</Tabs.Panel>
+							<Tabs.Panel></Tabs.Panel>
+						</Tabs.Panels>
+					</Tabs>
 				</div>
-				<aside className="sticky top-18 h-[calc(100vh-90px)] bg-(--background-color) border-t border-(--border-color)/30">
-					<NavigationMenu
-						data={data}
-						currentPath={"#" + activeId || ""}
-						alwaysOpen
-						showLines
-						styleLines="dotted"
-					/>
-				</aside>
 			</div>
+			<BarNavButton buttonLeft={buttonLeft} buttonRight={buttonRight} />
+			<FooterDocs />
 		</section>
 	);
 };
