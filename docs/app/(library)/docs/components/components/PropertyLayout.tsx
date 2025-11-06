fee92f105f1f20fddc4b41896fbc33ce191dc05e
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-11-05 22:44:43 -0600

    feat(PropertyLayout): enhance layout with additional props and improved structure

--- docs/app/(library)/docs/components/components/PropertyLayout.tsx
+++ docs/app/(library)/docs/components/components/PropertyLayout.tsx
@@ -1,26 +1,70 @@
-import { CodeBlock } from "lambda-ui-components";
+import { CodeBlock, Divider, Tabs } from "lambda-ui-components";
 import "prismjs/components/prism-jsx";
 import "prismjs/components/prism-typescript";
 import "prismjs/components/prism-tsx";
+import { Code2Icon, EyeIcon } from "lucide-react";
 interface PropertyLayoutProps {
-	children: React.ReactNode;
+	children?: React.ReactNode;
 	title: string;
+	description?: string;
+	propertyName?: string;
 	id?: string;
 	code?: string;
 }
 
-export default function PropertyLayout({ children, title, id, code }: PropertyLayoutProps) {
+export default function PropertyLayout({
+	children,
+	title,
+	description,
+	propertyName,
+	id,
+	code,
+}: PropertyLayoutProps) {
 	return (
-		<article className="prose prose-neutral dark:prose-invert max-w-none prose-headings:scroll-mt-24 prose-a:text-(--primary-base-color) pr-3">
-			<h2
-				id={id}
-				className="text-2xl font-bold text-left tracking-tight text-(--foreground-color) mb-4 w-full scroll-mt-20"
-			>
-				{title}
-			</h2>
-			<div className="w-full text-base text-(--foreground-secondary-color)">{children}</div>
-			{code && <CodeBlock code={code} language="tsx" />}
-			<hr className="my-10 border-(--border-color)" />
+		<article className="pr-3 my-4">
+			<div className="flex flex-col gap-1 mb-10">
+				<h2
+					id={id}
+					className="flex items-center text-2xl font-bold text-left tracking-tight text-(--foreground-color) w-full scroll-mt-20"
+				>
+					{title}
+					{propertyName && (
+						<span
+							className="font-mono text-(--foreground-secondary-color) text-xs ml-2 
+						opacity-80 bg-(--surface-c)/45 px-1 py-0.5 rounded-xs border border-(--surface-c)"
+						>
+							{propertyName}
+						</span>
+					)}
+				</h2>
+				{description && (
+					<p className="text-sm leading-relaxed text-(--foreground-secondary-color) mt-1 max-w-prose">
+						{description}
+					</p>
+				)}
+			</div>
+
+			{code && (
+				<Tabs variant="border" size="small" radius="small" color="primary">
+					<Tabs.List>
+						{children && <Tabs.Tab title="Preview" icon={<EyeIcon />} />}
+						<Tabs.Tab title="Code" icon={<Code2Icon />} />
+					</Tabs.List>
+					<Tabs.Panels>
+						{children && (
+							<Tabs.Panel>
+								<div className="w-full text-base text-(--foreground-secondary-color)">
+									{children}
+								</div>
+							</Tabs.Panel>
+						)}
+						<Tabs.Panel>
+							<CodeBlock code={code} language="tsx" buttonCopy />
+						</Tabs.Panel>
+					</Tabs.Panels>
+				</Tabs>
+			)}
+			<Divider spacing={70} variant="dashed" className="opacity-80" />
 		</article>
 	);
 }
