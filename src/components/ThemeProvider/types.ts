7995f590363a86fa7e39330cf688c60c217d012f
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-11-26 14:32:53 -0600

    feat(styles): add news themes "sunset" , "ocean" and "midnight";

--- src/components/ThemeProvider/types.ts
+++ src/components/ThemeProvider/types.ts
@@ -33,8 +33,8 @@ export interface UseThemeProps {
 
 export type Attribute = DataAttribute | "class";
 
-export type LightTheme = "light" | "retro" | "lavender" | "mint";
-export type DarkTheme = "dark" | "slate" | "deep-cosmic-night" | "soft-obsidian" | "graphite";
+export type LightTheme = "light" | "retro" | "lavender" | "mint" | "sunset" | "ocean";
+export type DarkTheme = "dark" | "slate" | "deep-cosmic-night" | "soft-obsidian" | "graphite" | "midnight";
 export type AllThemes = LightTheme | DarkTheme | "system";
 
 export type LambdaColor =
