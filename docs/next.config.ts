c10e9033bc15a68b3d63d2389872756c739b0555
BitCoder <alejo_2986@hotmail.com>
2026-07-30 23:49:26 -0600

    fix: tsconfig script and netlify build config

--- docs/next.config.ts
+++ docs/next.config.ts
@@ -1,13 +1,8 @@
 import type { NextConfig } from "next";
 
-const isGithubActions = process.env.GITHUB_ACTIONS === "true";
-const basePath = isGithubActions ? "/lambda-ui-components" : "";
-
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
-  basePath: basePath,
-  assetPrefix: basePath,
   typescript: {
     ignoreBuildErrors: true,
   },
